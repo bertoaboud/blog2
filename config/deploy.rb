718f655d690a3df872e83ffe6bb80687a779904d
humberto <humbertoaboud@gmail.com>
2013-01-04 10:17:12 -0200

    blog v2

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,9 +45,51 @@ namespace :deploy do
   task :update_code, :except => { :no_release => true } do
     on_rollback { run "rm -rf #{release_path}; true" }
     strategy.deploy!
-    run "echo === Rake Setup=== && cd #{latest_release} && bundle exec rake db:setup RAILS_ENV=production"
+    run "echo === Rake Setup=== && cd #{latest_release} && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production"
   end
   task :after_deploy do
 #    cleanup
   end
 end
+
+#restart APACHE
+namespace :apache do
+  [:stop, :start, :restart, :reload].each do |action|
+    desc "#{action.to_s.capitalize} Apache"
+    task action, :roles => :web do
+      invoke_command "/etc/init.d/apache2 #{action.to_s}", :via => run_method
+    end
+  end
+end
+
+
+namespace :deploy do
+  namespace :assets do
+ 
+    def not_first_deploy?
+      'true' ==  capture("if [ -e #{current_path}/REVISION ]; then echo 'true'; fi").strip
+    end
+ 
+    desc "Run the asset precompilation rake task only if there are changes."
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      if not_first_deploy?
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+        else
+          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        end
+      end
+    end
+ 
+  end
+end
+
+
+namespace :deploy do
+  task :bundle do
+    run "cd #{latest_release} && bundle install --deployment --without development test staging"
+  end
+end
+
+after "deploy", "deploy:assets:precompile"

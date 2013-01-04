e79730af0b0311bcc12aa2153ab9e6b7674bd5e3
humberto <humbertoaboud@gmail.com>
2013-01-04 12:14:24 -0200

    blog v3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,10 @@
-require "rvm/capistrano"
-require "bundler/capistrano"
-
-#load "deploy/assets"
-#load 'lib/deploy/assets'
-
+#----------------------------------------------------
 # replace these with your server's information
 set :domain,  "vonneumann.avante.com"
 set :user,    "deploy"
 
-# name this the same thing as the directory on your server
 set :application, "blog"
-
-# use your local repository as the source
 set :repository, "git@github.com:bertoaboud/blog2.git"
-
-# or use a hosted repository
-#set :repository, "ssh://user@example.com/~/git/test.git"
 
 server "#{domain}", :app, :web, :db, :primary => true
 
@@ -23,73 +12,32 @@ set :deploy_via, :copy
 set :copy_exclude, [".git", ".DS_Store"]
 set :scm, :git
 set :branch, "master"
-# set this path to be correct on yoru server
+
 set :deploy_to, "/home/#{user}/#{application}"
 set :use_sudo, false
 set :keep_releases, 2
 set :git_shallow_clone, 1
 
 ssh_options[:paranoid] = false
+#----------------------------------------------------
 
-# this tells capistrano what to do when you deploy
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :default_environment, {
+  'GEM_HOME'     => '/home/deploy/.rvm/gems/ruby-1.9.3-p327',
+  'GEM_PATH'     => '/home/deploy/.rvm/gems/ruby-1.9.3-p327',
+  'BUNDLE_PATH'  => '/home/deploy/.rvm/gems/ruby-1.9.3-p327'
+}
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-  desc <<-DESC
-  A macro-task that updates the code and fixes the symlink.
-  DESC
-  task :default do
-    transaction do
-      update_code
-      symlink
-    end
-  end
-  task :update_code, :except => { :no_release => true } do
-    on_rollback { run "rm -rf #{release_path}; true" }
-    strategy.deploy!
-#    run "echo === Rake Setup=== && cd #{latest_release} && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production"
-  end
-  task :after_deploy do
-#    cleanup
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-
-#restart APACHE
-namespace :apache do
-  [:stop, :start, :restart, :reload].each do |action|
-    desc "#{action.to_s.capitalize} Apache"
-    task action, :roles => :web do
-      invoke_command "/etc/init.d/apache2 #{action.to_s}", :via => run_method
-    end
-  end
-end
-
-
-namespace :deploy do
-  namespace :assets do
- 
-    def not_first_deploy?
-      'true' ==  capture("if [ -e #{current_path}/REVISION ]; then echo 'true'; fi").strip
-    end
- 
-    desc "Run the asset precompilation rake task only if there are changes."
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      if not_first_deploy?
-        from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-        else
-          logger.info "Skipping asset pre-compilation because there were no asset changes"
-        end
-      end
-    end
- 
-  end
-end
-
-
-namespace :deploy do
-  task :bundle do
-    run "cd #{latest_release} && bundle install --deployment --without development test staging"
-  end
-end
-
-#after "deploy", "deploy:assets:precompile"

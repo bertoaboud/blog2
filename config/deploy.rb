2503a73b0fa91e43fd348bbe8305987534c5e692
humberto <humbertoaboud@gmail.com>
2013-01-04 15:30:40 -0200

    blog v6

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,24 +26,15 @@ require "bundler/capistrano"
 
 set :normalize_asset_timestamps, false
 
- #set :default_environment, {
- #  'GEM_HOME'     => '/home/deploy/.rvm/gems/ruby-1.9.3-p327',
- #  'GEM_PATH'     => '/home/deploy/.rvm/gems/ruby-1.9.3-p327',
- #  'BUNDLE_PATH'  => '/home/deploy/.rvm/gems/ruby-1.9.3-p327'
- #}
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
   task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
+#  task :restart, :roles => :app, :except => { :no_release => true } do
+#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#  end
 end
 
+after "deploy:assets:precompile", "db:create"
 
 namespace :db do
   desc "Create Production Database"
